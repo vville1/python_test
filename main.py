f9f713f9f87e48ce65815caab2ad9375de0e0afa
vville1 <vville1@yahoo.com>
2025-01-16 15:44:42 -0700

    Second commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def in_autotests_we_trust(a, b):
     if a == b:
-        print('Test Passed')
+        print('PASS')
     else:
-        print('Test Failed')
+        print('FAIL')
 
 in_autotests_we_trust(10, '10')
 
